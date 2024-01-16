e623fd6d8cafdcc1f00be7b408c8058d80b61e54
SHUBH SINGH <90331406+soshubh@users.noreply.github.com>
2024-01-16 16:47:22 +0530

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ import AutoComplete from './AutoComplete';
 const Home: React.FC = () => {
   return (
     <div className="container flex flex-col max-w-screen-xl items-center pt-20 h-screen bg-slate-100">
-      <h1>Pick User</h1>
+       <h1 className="text-4xl font-bold text-gray-900 mb-4">Pick User</h1>
       <AutoComplete />
     </div>
   );
